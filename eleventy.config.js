8d8fce6ca25dc3d627fda8869b6135f61819734a
Dash Vallance <dash@intelics.com.au>
2025-07-19 15:26:57 +1000

    site: add support for a series index

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,26 @@
 import markdownIt from "markdown-it";
-export default function(eleventyConfig) {
+
+export default (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy({"src/assets": "."});
 
-  eleventyConfig.addCollection("posts", function(collectionApi) {
+  eleventyConfig.addCollection("posts", (collectionApi) => {
     return collectionApi.getFilteredByGlob("src/posts/*.md");
   });
 
   const md = new markdownIt();
-  eleventyConfig.addPairedLiquidShortcode("note", function(content) {
+  eleventyConfig.addPairedLiquidShortcode("note", (content) => {
     return `<div class="note">${md.renderInline(content.trim())}</div>`;
+  });
+
+  eleventyConfig.addFilter("getSeriesPosts", (collectionApi, currentSeries) => {
+    if (!currentSeries) return [];
+    return collectionApi.all
+      .filter(post => post.data.series === currentSeries)
+      .sort((a, b) => new Date(a.data.date) - new Date(b.data.date))
+      .map((post, index) => ({
+        ...post,
+        seriesIndex: index + 1
+      }));
   });
 
   return {
